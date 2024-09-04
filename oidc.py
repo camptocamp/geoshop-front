b13042477a353437e1de93aded1b46eb484834d8
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-09-04 09:56:43 +0200

    Do not try to parse role information from the Zitadel

--- oidc.py
+++ oidc.py
@@ -3,31 +3,20 @@ from django.contrib.auth.models import Permission, User
 from django.contrib import admin
 from django.shortcuts import render
 
-ADMIN_ROLE = "admin"
-STAFF_ROLE = "admin"
-
-def updateUser(user, claims, roles):
+def updateUser(user, claims):
     user.email = claims.get("email")
     user.first_name = claims.get("given_name")
     user.last_name = claims.get("family_name")
-    user.is_superuser = ADMIN_ROLE in roles
-    user.is_staff = STAFF_ROLE in roles
     return user
 
 class PermissionBackend(OIDCAuthenticationBackend):
 
-    def get_roles(self, claims):
-        claim = f"urn:zitadel:iam:org:project:{self.get_settings('ZITADEL_PROJECT')}:roles"
-        return claims.get(claim, {})
-
     def create_user(self, claims):
-        roles = self.get_roles(claims)
-        user = self.UserModel.objects.create_user(username=claims.get("sub"))
-        updateUser(user, claims, roles)
+        user = self.UserModel.objects.create_user(username=claims.get("email"))
+        updateUser(user, claims)
         user.save()
         return user
 
     def update_user(self, user, claims):
-        roles = self.get_roles(claims)
-        updateUser(user, claims, roles).save()
+        updateUser(user, claims).save()
         return user
