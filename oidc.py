073479d77a7fecd2d9ce71f0802e670e7f67b03d
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-09-19 09:04:01 +0200

    Commit before Django Rest OAuth experiment

--- oidc.py
+++ oidc.py
@@ -13,17 +13,20 @@ from django.conf import settings
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login
-from django.http import JsonResponse,HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import get_user_model
 
 UserModel = get_user_model()
 
-KEY_FILE="/home/arusakov/devel/c2c/extract/geoshop-back/user_key.json"
+KEY_FILE = "/home/arusakov/devel/c2c/extract/geoshop-back/user_key.json"
+ZITADEL_OIDC_DOMAIN = "https://geoshop-demo-syazg0.zitadel.cloud"
+ZITADEL_INTROSPECT_URL = f"{ZITADEL_OIDC_DOMAIN}/oauth/v2/introspect"
 
 def status(request):
     return {
         "OIDC_ENABLED": settings.OIDC_ENABLED,
     }
+
 
 def updateUser(user, claims):
     user.email = claims.get("email")
@@ -37,51 +40,52 @@ def private_key():
         return {
             "client_id": data["clientId"],
             "key_id": data["keyId"],
-            "private_key": data["key"]
+            "private_key": data["key"],
         }
+
+def get_jwt_token(oidc_domain: str):
+    prk = private_key()
+    payload = {
+        "iss": prk["client_id"],
+        "sub": prk["client_id"],
+        "aud": oidc_domain,
+        "exp": int(time.time() + 3600),
+        "iat": int(time.time()),
+    }
+    return jwt.encode(
+        {"alg": "RS256", "kid": prk["key_id"]}, payload, prk["private_key"]
+    )
 
 @csrf_exempt
 def validate_token(request):
     # Handle JSON error
-    data = json.loads(request.body.decode("utf-8"))
-    prk = private_key()
     # TODO: Test missing id token
     # TODO: Localize error response
     # TODO: Test same token multiple times
+    data = json.loads(request.body.decode("utf-8"))
     if "token" not in data:
         return JsonResponse({"error": "No token provided"}, status=400)
 
-    token_string = data["token"]
-    url = "https://geoshop-demo-syazg0.zitadel.cloud/oauth/v2/introspect"
-    payload = {
-        "iss": prk["client_id"],
-        "sub": prk["client_id"],
-        "aud": "https://geoshop-demo-syazg0.zitadel.cloud",
-        "exp": int(time.time() + 3600),
-        "iat": int(time.time())
-    }
-    header = {"alg": "RS256", "kid": prk["key_id"]}
-    jwt_token = jwt.encode(header, payload, prk["private_key"])
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-
-    data = {
-        "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-        "client_assertion": jwt_token,
-        "token": token_string,
-    }
-    resp = requests.post(url, headers=headers, data=data)
+    resp = requests.post(
+        ZITADEL_INTROSPECT_URL,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data={
+            "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+            "client_assertion": get_jwt_token(ZITADEL_OIDC_DOMAIN),
+            "token": data["token"],
+        },
+    )
     resp.raise_for_status()
     user_data = json.loads(resp.content)
 
     UserModel = get_user_model()
-
     user = UserModel.objects.get(username=user_data["email"])
     if not user:
         user = UserModel.objects.create_user(username=user_data["email"])
     updateUser(user, user_data)
     user.save()
 
-    return JsonResponse({"user": serializers.serialize("json", [user])})
+    return JsonResponse({"user": {"id": user.id, "username": user.username}})
 
 
 class PermissionBackend(OIDCAuthenticationBackend):
