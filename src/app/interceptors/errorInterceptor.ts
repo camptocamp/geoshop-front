e94ac29ee223171daa1c928e3f1cf2930e3a85c8
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-04-15 14:59:12 +0200

    Added error interceptor

--- src/app/interceptors/errorInterceptor.ts
+++ src/app/interceptors/errorInterceptor.ts
@@ -1,15 +1,13 @@
 import { AppState } from '@app/store';
 import * as fromAuth from '@app/store/auth/auth.action';
 
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpRequest, HttpEvent, HttpErrorResponse, HttpHandlerFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
-
 
 function formatArea(area: number): string {
   return area > 100000 ?
@@ -40,43 +38,37 @@ function formatGenericError(err: Record<string, unknown>): string {
   return messages.join("\n");
 }
 
-@Injectable()
-export class ErrorInterceptor implements HttpInterceptor {
-
-  constructor(private store: Store<AppState>, private router: Router, private snackBar: MatSnackBar) {
-  }
-
-  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-    return next.handle(req).pipe(
-      catchError(response => {
-        if (response instanceof HttpErrorResponse) {
-          if (response.status === 401) {
-            this.store.dispatch(fromAuth.logout());
-            this.router.navigate(['/auth/login']);
+export const interceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+  const snackBar = inject(MatSnackBar);
+  const store = inject(Store<AppState>);
+  const router = inject(Router);
+  return next(req).pipe(
+    catchError(response => {
+      if (response instanceof HttpErrorResponse) {
+        if (response.status === 401) {
+          store.dispatch(fromAuth.logout());
+          router.navigate(['/auth/login']);
+        } else {
+          let message = "";
+          const err = response.error;
+          // TODO: Better error type recognition
+          if (!err.message && response.url) {
+            message = formatConnectionError(response);
+          } else if (Array.isArray(err.message) && err.message[0] === 'Order area is too large') {
+            message = formatAreaError(err);
+          } else if (JSON.stringify(err.message).toLowerCase().indexOf("token expired") !== -1) {
+            message = $localize`La session est expirée`;
           } else {
-            let message = "";
-            const err = response.error;
-            // TODO: Better error type recognition
-            if (!err.message && response.url) {
-              message = formatConnectionError(response);
-            } else if (Array.isArray(err.message) && err.message[0] === 'Order area is too large') {
-              message = formatAreaError(err);
-            } else if (JSON.stringify(err.message).toLowerCase().indexOf("token expired") !== -1) {
-              message = $localize`La session est expirée`;
-            } else {
-              message = formatGenericError(err);
-            }
-            if (message.length === 0) {
-              message = response.message;
-            }
-            this.snackBar.open(message, 'Ok', { panelClass: 'notification-error' });
+            message = formatGenericError(err);
           }
+          if (message.length === 0) {
+            message = response.message;
+          }
+          snackBar.open(message, 'Ok', { panelClass: 'notification-error' });
         }
+      }
 
-        return throwError(response);
-      })
-    );
-  }
-
+      return throwError(response);
+    }));
 }
+
