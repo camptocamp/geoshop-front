68d332ec1da22028d70b5583de926f4c58d117c1
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-05-26 15:37:59 +0200

    Save map state in the state instead of direct localStorage

--- src/app/_store/index.ts
+++ src/app/_store/index.ts
@@ -1,20 +1,24 @@
-import {AuthState} from './auth/auth.reducer';
-import {ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer} from '@ngrx/store';
-import {storeFreeze} from 'ngrx-store-freeze';
-import {environment} from '../../environments/environment';
+import { AuthState } from './auth/auth.reducer';
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../../environments/environment';
 import * as fromAuth from './auth/auth.reducer';
-import {CartState} from './cart/cart.reducer';
+import { CartState } from './cart/cart.reducer';
 import * as fromCart from './cart/cart.reducer';
-import {storageMetaReducer} from './storage.reducer';
+import { storageMetaReducer } from './storage.reducer';
+import * as fromMap from './map/map.reducer';
+import { MapState } from './map/map.reducer';
 
 export interface AppState {
   auth: AuthState;
   cart: CartState;
+  map: MapState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
   auth: fromAuth.reducer,
   cart: fromCart.reducer,
+  map: fromMap.reducer,
 };
 
 export function logger(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
@@ -56,4 +60,11 @@ export const selectCartTotal = createSelector(
 export const selectOrder = createSelector(
   cartFeatureSelector,
   fromCart.selectOrder
+);
+
+// Map store
+export const mapFeatureSelector = createFeatureSelector<fromMap.MapState>('map');
+export const selectMapState = createSelector(
+  mapFeatureSelector,
+  (state: fromMap.MapState) => state
 );
