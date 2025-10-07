30173a487e5bd6dfac951b8d356d4ddd8a902e22
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2025-10-07 14:10:53 +0200

    Disable error interception if token is not found (error 404 on /validate/orderitem/{token} )

--- src/app/interceptors/errorInterceptor.ts
+++ src/app/interceptors/errorInterceptor.ts
@@ -54,7 +54,12 @@ export const interceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn): Obs
           let message = "";
           const err = response.error;
           // TODO: Better error type recognition
-          if (!err.message && response.url) {
+          // the item below is checked with highest priority
+          // TODO allow exceptions: expected API errors should not lead to an error in snack bar
+          if (response.url?.includes('validate/orderitem') && (response.status == 404)) {
+            message = $localize`Token not found`;
+            return throwError(response);;
+          } else if (!err.message && response.url) {
             message = formatConnectionError(response);
           } else if (Array.isArray(err.message) && err.message[0] === 'Order area is too large') {
             message = formatAreaError(err);
